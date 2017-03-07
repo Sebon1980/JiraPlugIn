194113467de85e1c8f67e7f2cb35ca03e14cf327
Sebon1980 <sebon1980@outlook.de>
2017-03-07 10:06:57 +0100

    complete parseIssue Object

--- Jira/helper/parseIssue.js
+++ Jira/helper/parseIssue.js
@@ -4,16 +4,22 @@ module.exports = function parseIssue(data) {
 
     data.issues.forEach((currentIssue) => {
         var issue = {
-            id: currentIssue.id,
-            key: currentIssue.key,
-            adress: currentIssue.self,
+            issueId: currentIssue.id,
+            issueKey: currentIssue.key,
             summary: currentIssue.fields.summary,
-            sprintId: currentIssue.fields.sprint ? currentIssue.fields.sprint.id : 'not listed in sprint',
-            epicId: currentIssue.fields.epic ? currentIssue.fields.epic.id : 'not listed in epic',
-            assigneeName: currentIssue.fields.assignee ? currentIssue.fields.assignee.displayName : 'no assignee',
-            epicName: currentIssue.fields.epic.name,
+            issueAdress: currentIssue.self,
             status: currentIssue.fields.status.name,
-            isDone: currentIssue.fields.status.name == 'Done'
+            isDone: currentIssue.fields.status.name == 'Done',
+            versionId: currentIssue.fields.fixVersions ? currentIssue.fields.fixVersions[0].id : 'not signed to any version',
+            versionName: currentIssue.fields.fixVersions ? currentIssue.fields.fixVersions[0].name : 'not signed to any version',
+            versionAdress: currentIssue.fields.fixVersions ? currentIssue.fields.fixVersions[0].self : 'not signed to any version',
+            epicId: currentIssue.fields.epic ? currentIssue.fields.epic.id : 'not listed in epic',
+            epicName: currentIssue.fields.epic.name,
+            sprintId: currentIssue.fields.sprint ? currentIssue.fields.sprint.id : 'not listed in sprint',
+            sprintName: currentIssue.fields.sprint ? currentIssue.fields.sprint.name : 'not listed in sprint',
+            sprintAdress: currentIssue.fields.sprint ? currentIssue.fields.sprint.self : 'not listed in sprint',
+            assigneeName: currentIssue.fields.assignee ? currentIssue.fields.assignee.displayName : 'no assignee',
+
         }
         Issues.push(issue);
     })
