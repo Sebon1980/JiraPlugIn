9dd2cb9512d4b2b257ff0ee4279dbeb4be0724f3
Sebon1980 <sebon1980@outlook.de>
2017-03-17 13:57:19 +0100

    refactoring

--- scripts/Jira/Jira.js
+++ scripts/Jira/Jira.js
@@ -9,45 +9,77 @@ class Jira {
         this.connector = new JiraClient(config);
     }
 
+    /**
+     * function to get all Boards of specified Jira account
+     * by using npm module "jira-connector"
+     */
     getAllBoards() {
-        var opts = {
-            type: "",
-            startAt: 0,
-            maxResults: 25
-        };
-        return this.connector.board.getAllBoards(opts)
-    }
+            var opts = {
+                type: "",
+                startAt: 0,
+                maxResults: 25
+            };
+            return this.connector.board.getAllBoards(opts)
+        }
+        /**
+         * 
+         * @param {*} boardId 
+         * GET method to return Details of named Board 
+         */
 
     getDetailsOfBoard(boardId) {
-        var opts = {
-            boardId,
-            type: "",
-            startAt: 0,
-            maxResults: 25
-        };
-        return this.connector.board.getBoard(opts)
-    }
+            var opts = {
+                boardId,
+                type: "",
+                startAt: 0,
+                maxResults: 25
+            };
+            return this.connector.board.getBoard(opts)
+        }
+        /**
+         * GET Method returns all Issues of named board
+         * by using the boardId
+         * @param {*} boardId 
+         */
 
     getIssuesOfBoard(boardId) {
-        var opts = {
-            boardId,
-            type: "",
-            startAt: 0,
-            maxResults: 25
-        };
-        return this.connector.board.getIssuesForBoard(opts)
-            .then(result => parseIssue(result))
-    }
+            var opts = {
+                boardId,
+                type: "",
+                startAt: 0,
+                maxResults: 25
+            };
+            return this.connector.board.getIssuesForBoard(opts)
+                .then(result => parseIssue(result))
+        }
+        /**
+         * GET Method to return all Versions with details of named board
+         * by using the boardId
+         * @param {*} boardId 
+         */
 
-    getAllVersions(projectIdOrKey) {
-        var opts = {
-            projectIdOrKey,
-            type: "",
-            startAt: 0,
-            maxResults: 25
-        };
-        return this.connector.project.getVersions(opts)
-    }
+    getVersions(boardId) {
+            var opts = {
+                boardId,
+                type: "",
+                startAt: 0,
+                maxResults: 25
+            };
+
+            return this.connector.version.getAllVersions(opts)
+                .then(result => {
+                    let versionList = [];
+                    result.values.forEach(value => {
+                        versionList.push(this.getDetailsOfVersion(value.id))
+                    });
+                    return Promise.all(versionList)
+                })
+        }
+        /**
+         * GET Method to return the details of named version 
+         * by using the versionId
+         * @param {*} versionId 
+         */
 
     getDetailsOfVersion(versionId) {
         var opts = {
@@ -59,6 +91,14 @@ class Jira {
         return this.connector.version.getVersion(opts)
     }
 
+    /**
+     * Get method to return all Issues 
+     * which belongs to a named board
+     * @param {*} boardId 
+     * and be selected by specific key/value pair
+     * @param {*} params 
+     */
+
     selectIssuesBy(boardId, params) {
 
         return this.getIssuesOfBoard(boardId)
@@ -68,18 +108,13 @@ class Jira {
             .catch(err => console.log(err))
 
     }
+
+    /**
+     * Function get calculate and return the progress 
+     * of the given Isuues in @param {*} data
+     */
     getStatus(data) {
         return getStatus(data)
-    }
-
-    getVersions(boardId) {
-        var opts = {
-            boardId,
-            type: "",
-            startAt: 0,
-            maxResults: 25
-        };
-        return this.connector.version.getAllVersions(opts)
     }
 
     getProgressOfVersionsInBoard(boardId) {
@@ -88,7 +123,7 @@ class Jira {
             this.getVersions(boardId)
                 .then((versions) => {
                     let versionList = [];
-                    versions.values.forEach(version => {
+                    versions.forEach(version => {
                         versionList.push(this.getDetailsOfVersion(version.id))
                     });
 
@@ -113,9 +148,28 @@ class Jira {
                     console.log(err);
                 })
         })
+    }
 
+    getVersionProgress(boardId, versionId) {
+        return new Promise((resolve, reject) => {
+
+                this.selectIssuesBy(boardId, { versionId: version.id })
+                return Promise.all(issueList);
+            })
+            .then(issueList => {
+                let progressList = [];
+                issueList.forEach(issue => {
+                    progressList.push(this.getStatus(issue))
+                });
+                resolve(progressList)
+            })
+            .catch(err => {
+                reject(err);
+                console.log(err);
+            })
     }
 }
+
 
 
 module.exports = Jira;
--- scripts/app.js
+++ scripts/app.js
@@ -5,14 +5,13 @@ const exphbs = require('express-handlebars');
 var expressJoi = require('express-joi');
 
 const app = express();
-
-app.use(require('body-parser').json()); // parse body params and attache them to req.body
-app.use(require('body-parser').urlencoded({ extended: true }));
-
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const testJira = new Jira(config.jira);
+app.use(require('body-parser').json()); // parse body params and attache them to req.body
+app.use(require('body-parser').urlencoded({ extended: true }));
+
 
 var Joi = expressJoi.Joi;
 var test = {
@@ -26,15 +25,50 @@ var test = {
 
 app.get('/show/versions/:boardId', function(req, res) {
 
-    testJira.getProgressOfVersionsInBoard(req.params.boardId)
+    testJira.getVersions(req.params.boardId)
+        .then(versions => {
+            console.log(versions)
+            res.send(versions)
+        })
+        .catch(err => {
+            res.send(err)
+        })
+});
+
+app.get('/show/versions/details/:versionId', function(req, res) {
+    testJira.getDetailsOfVersion(req.params)
+        .then(version => {
+            console.log(version)
+            res.send(version)
+        })
+        .catch(err => {
+            res.send(err)
+        })
+})
+
+app.get('/show/versions/progress/:boardId/:versionId', function(req, res) {
+
+    testJira.getProgressOfVersionsInBoard(req.params.boardId, req.params.versionId)
         .then(progress => {
             console.log(progress)
             res.send(progress)
         })
         .catch(err => {
-
+            res.send(err)
         })
-})
+});
+
+app.get('/show/versions/progress/:boardId/:versionId', function(req, res) {
+
+    testJira.getVersionProgress(req.params.boardId, req.params.versionId)
+        .then(progress => {
+            console.log(progress)
+            res.send(progress)
+        })
+        .catch(err => {
+            res.send(err)
+        })
+});
 
 app.listen(port, () => {
     console.log(`Server is running at http://${hostname}:${port}/`);
