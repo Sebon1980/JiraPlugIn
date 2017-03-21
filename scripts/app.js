24c3d68ef93b3b05650842f9a3c77175c09193dc
Sebon1980 <sebon1980@outlook.de>
2017-03-21 10:53:25 +0100

    update

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 const Jira = require('./Jira/Jira')
 const config = require('./config');
 const express = require('express');
-const exphbs = require('express-handlebars');
+const hbs = require('express-handlebars');
 var expressJoi = require('express-joi');
 
 const app = express();
@@ -9,6 +9,8 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const testJira = new Jira(config.jira);
+
+
 app.use(require('body-parser').json()); // parse body params and attache them to req.body
 app.use(require('body-parser').urlencoded({ extended: true }));
 
@@ -23,6 +25,19 @@ var test = {
     status: Joi.types.string()
 };
 
+app.get('/show/allIssues/:boardId', function(req, res, next) {
+    testJira.selectIssuesBy(req.params.boardId, req.query)
+        .then(result => {
+            if (result.length === 0) {
+                console.log('Select parameter doesn´t exist')
+                return res.send('Select parameter doesn´t exist')
+            }
+            res.render('overview', result);
+            console.log(req.query)
+        })
+        .catch(e => console.log(e));
+})
+
 app.get('/show/versions/:boardId', function(req, res) {
 
     testJira.getVersions(req.params.boardId)
@@ -36,7 +51,7 @@ app.get('/show/versions/:boardId', function(req, res) {
 });
 
 app.get('/show/versions/details/:versionId', function(req, res) {
-    testJira.getDetailsOfVersion(req.params)
+    testJira.getDetailsOfVersion(req.params.versionId)
         .then(version => {
             console.log(version)
             res.send(version)
